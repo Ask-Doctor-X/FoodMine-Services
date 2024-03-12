d802e44c337cfc311151ab5d2625ff4bb01227e6
Ask-Doctor-X <subrah1994@gmail.com>
2024-03-12 16:37:41 +0100

    First commit for FoodMine api services

---
+++ src/main/java/com/FoodMineServices/FoodMineServices/FoodMineServicesApplication.java
@@ -0,0 +1,13 @@
+package com.FoodMineServices.FoodMineServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodMineServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodMineServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FoodMineServices/FoodMineServices/FoodMineServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FoodMineServices.FoodMineServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodMineServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
